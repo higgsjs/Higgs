396dd689c176bf89cebea693a9bc002089222855
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2013-09-04 16:48:02 -0400

    - Improvements to benchmark script

--- source/benchmark.py
+++ source/benchmark.py
@@ -3,43 +3,42 @@
 from subprocess import *
 import re
 
+# Configuration
 MAKE_CMD = 'make release'
-
 HIGGS_CMD = './higgs --stats'
+NUM_RUNS = 5
 
-NUM_RUNS = 10
+BENCHMARKS = {
+    '3d-cube':'programs/sunspider/3d-cube.js',
+    '3d-morph':'programs/sunspider/3d-morph.js',
+    '3d-raytrace':'programs/sunspider/3d-raytrace.js',
+    'binary-trees':'programs/sunspider/access-binary-trees.js',
+    'fannkuch':'programs/sunspider/access-fannkuch.js',  
+    'nbody':'programs/sunspider/access-nbody.js',      
+    'nsieve':'programs/sunspider/access-nsieve.js',
+    '3bit':'programs/sunspider/bitops-3bit-bits-in-byte.js',
+    'bits-in-byte':'programs/sunspider/bitops-bits-in-byte.js',  
+    'bitwise-and':'programs/sunspider/bitops-bitwise-and.js',       
+    'nsieve-bits':'programs/sunspider/bitops-nsieve-bits.js',        
+    'recursive':'programs/sunspider/controlflow-recursive.js',
+    'crypto-aes':'programs/sunspider/crypto-aes.js',     
+    'crypto-md5':'programs/sunspider/crypto-md5.js',
+    'crypto-sha1':'programs/sunspider/crypto-sha1.js',
+    'cordic':'programs/sunspider/math-cordic.js',
+    'partial-sums':'programs/sunspider/math-partial-sums.js',
+    'spectral-norm':'programs/sunspider/math-spectral-norm.js',
+    'fasta':'programs/sunspider/string-fasta.js',
 
-BENCHMARKS = [
-    'programs/sunspider/3d-cube.js',
-    'programs/sunspider/3d-morph.js',
-    'programs/sunspider/3d-raytrace.js',
-    'programs/sunspider/access-binary-trees.js',
-    'programs/sunspider/access-fannkuch.js',  
-    'programs/sunspider/access-nbody.js',      
-    'programs/sunspider/access-nsieve.js',
-    'programs/sunspider/bitops-3bit-bits-in-byte.js',
-    'programs/sunspider/bitops-bits-in-byte.js',  
-    'programs/sunspider/bitops-bitwise-and.js',       
-    'programs/sunspider/bitops-nsieve-bits.js',        
-    'programs/sunspider/controlflow-recursive.js',
-    'programs/sunspider/crypto-aes.js',     
-    'programs/sunspider/crypto-md5.js',
-    'programs/sunspider/crypto-sha1.js',
-    'programs/sunspider/math-cordic.js',
-    'programs/sunspider/math-partial-sums.js',
-    'programs/sunspider/math-spectral-norm.js',
-    'programs/sunspider/string-fasta.js',
+    'v8-crypto':'programs/v8bench/base.js programs/v8bench/crypto.js programs/v8bench/drv-crypto.js',
+    'deltablue':'programs/v8bench/base.js programs/v8bench/deltablue.js programs/v8bench/drv-deltablue.js',
+    'earley-boyer':'programs/v8bench/base.js programs/v8bench/earley-boyer.js programs/v8bench/drv-earley-boyer.js',
+    'navier-stokes':'programs/v8bench/base.js programs/v8bench/navier-stokes.js programs/v8bench/drv-navier-stokes.js',
+    'v8-raytrace':'programs/v8bench/base.js programs/v8bench/raytrace.js programs/v8bench/drv-raytrace.js',
+    'richards':'programs/v8bench/base.js programs/v8bench/richards.js programs/v8bench/drv-richards.js',
+}
 
-    'programs/v8bench/base.js programs/v8bench/crypto.js programs/v8bench/drv-crypto.js',
-    'programs/v8bench/base.js programs/v8bench/deltablue.js programs/v8bench/drv-deltablue.js',
-    'programs/v8bench/base.js programs/v8bench/earley-boyer.js programs/v8bench/drv-earley-boyer.js',
-    'programs/v8bench/base.js programs/v8bench/navier-stokes.js programs/v8bench/drv-navier-stokes.js',
-    'programs/v8bench/base.js programs/v8bench/raytrace.js programs/v8bench/drv-raytrace.js',
-    'programs/v8bench/base.js programs/v8bench/richards.js programs/v8bench/drv-richards.js',
-]
-
-# Dictionary of string keys to lists of gathered values
-values = {}
+# Per-benchmark results
+benchResults = {}
 
 # Compile Higgs in release mode
 call(MAKE_CMD, shell=True)
@@ -47,17 +46,21 @@ call(MAKE_CMD, shell=True)
 # Captured value pattern
 valPattern = re.compile('^(.+):(.+)$')
 
-# For each run
-for runNo in range(1, NUM_RUNS+1):
+# For each benchmark
+benchNo = 1
+for benchmark in BENCHMARKS:
 
-    print "Run #", runNo
+    benchFiles = BENCHMARKS[benchmark]
+    print '%s (%d / %d)' % (benchmark, benchNo, len(BENCHMARKS))
+    benchNo += 1
 
-    # For each benchmark
-    for benchIdx in range(0, len(BENCHMARKS)):
+    # Dictionary of string keys to lists of gathered values
+    valLists = {}
 
-        benchFiles = BENCHMARKS[benchIdx]
+    # For each run
+    for runNo in range(1, NUM_RUNS+1):
 
-        print benchFiles.split(' ')[-1], "(", (benchIdx+1), "/", len(BENCHMARKS), ")"
+        print 'Run #%d / %d' % (runNo, NUM_RUNS)
 
         # Run the benchmark and capture its output
         pipe = Popen(HIGGS_CMD + ' ' + benchFiles, shell=True, stdout=PIPE).stdout
@@ -76,17 +79,26 @@ for runNo in range(1, NUM_RUNS+1):
             val = float(match.group(2))
 
             # Add the value to the list for this key
-            if not (key in values):
-                values[key] = []
-            values[key] = values[key] + [val]
+            if not (key in valLists):
+                valLists[key] = []
+            valLists[key] = valLists[key] + [val]
 
-# TODO: compute averages
+    # Store the values for this benchmark
+    benchResults[benchmark] = valLists
+
+# Compute the geometric mean of values
+benchMeans = {}
+for benchmark in benchResults:
+
+    pass
 
 
 
 
 
 
+
+# TODO: output time stats, including mean time
 
 
 
