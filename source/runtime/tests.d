1102ff3a5310eba6e54f272859d51e8d3176334d
Tom Brasington <teebrz@gmail.com>
2014-01-27 11:51:04 -0500

    - fixed merge conflicts

--- source/runtime/tests.d
+++ source/runtime/tests.d
@@ -1349,6 +1349,8 @@ unittest
     vm.load("tests/core/regress/dowhile_cont.js");
     vm.load("tests/core/regress/vers_pathos.js");
 
+    vm.load("tests/regress/ir_string.js");
+
     vm.load("tests/core/regress/jit_se_cmp.js");
     vm.load("tests/core/regress/jit_float_cmp.js");
     vm.load("tests/core/regress/jit_getprop_arr.js");
