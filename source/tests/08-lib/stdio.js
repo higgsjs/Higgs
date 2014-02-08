950ddd6b02e53b617bf0e35a155dcf443debc397
Tom Brasington <teebrz@gmail.com>
2014-02-08 16:48:55 -0500

    - rename tests/00-runtime/runtime.js 	- tweak aforementioned test file 	- add readFile() to lib/stdio re: issue #71 	- add tests for fopen, write, readFile()

--- source/tests/08-lib/stdio.js
+++ source/tests/08-lib/stdio.js
@@ -1,5 +1,17 @@
-stdio = require('lib/stdio');
+var stdio = require('lib/stdio');
+var test_str = "This is only a test.";
 
-tmpName = stdio.tmpname();
+// tmpname
+var tmpName = stdio.tmpname();
 assert (typeof tmpName === "string" && tmpName.length > 0);
 
+// open/write
+var myFile = stdio.fopen(tmpName, "w+");
+myFile.write(test_str);
+
+assert(myFile.read() === test_str, "read/write ok");
+
+myFile.close();
+
+// readFile()
+assert (stdio.readFile(tmpName) === test_str, "readFile ok");
