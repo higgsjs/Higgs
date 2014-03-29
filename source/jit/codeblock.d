e4d5812dce8cc163e92fa9a34c2668cd0e8b773b
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2014-03-29 13:16:02 -0400

    - Minor fix

--- source/jit/codeblock.d
+++ source/jit/codeblock.d
@@ -38,10 +38,10 @@
 module jit.codeblock;
 
 import core.sys.posix.unistd;
-version(Posix)
-    import core.sys.posix.sys.mman;
-else version(linux)
+version(linux)
     import core.sys.linux.sys.mman;
+else version(Posix)
+    import core.sys.posix.sys.mman;
 import core.memory;
 import std.stdio;
 import std.array;
