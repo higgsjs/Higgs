214448124c316a93d5309ad2b9b48f7f4bb03e59
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2014-04-02 17:14:25 -0400

    - Fixed infinite loop bug in peephole IR optimizations

--- source/runtime/tests.d
+++ source/runtime/tests.d
@@ -1470,7 +1470,8 @@ unittest
     vm.load("tests/core/regress/dowhile_cont.js");
     vm.load("tests/core/regress/vers_pathos.js");
 
-    vm.load("tests/core/regress/ir_string.js");
+    vm.load("tests/core/regress/ir-string.js");
+    vm.load("tests/core/regress/ir-inf-loop.js");
 
     vm.load("tests/core/regress/jit_se_cmp.js");
     vm.load("tests/core/regress/jit_float_cmp.js");
