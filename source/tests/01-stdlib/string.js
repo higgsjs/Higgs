1adb5d585b52d1fb5100df3ecc0d83b1c4b4b952
yawnt <yawn.localhost@gmail.com>
2014-09-20 20:54:58 +0200

    fix string split, support regex

--- source/tests/01-stdlib/string.js
+++ source/tests/01-stdlib/string.js
@@ -273,7 +273,7 @@ function test_replace()
 
     if ('foobar foobar'.replace(/\bf/g, "$`") !== 'oobar foobar oobar')
         return 4;
-    
+
     if ('foobar foobar'.replace(/\bf/g, "$'") !== 'oobar foobaroobar oobaroobar')
         return 5;
     if ('f'.replace(/a/g, "b") !== 'f')
@@ -332,7 +332,10 @@ function test_split()
         return 12;
 
     if (!array_eq('181'.split([8]), ['1','1']))
-        return 12;
+        return 13;
+
+    if (!array_eq('181 181'.split(/\s/), ['181', '181']))
+        return 14;
 
     return 0;
 }
@@ -460,7 +463,7 @@ function test()
 }
 
 // TODO: convert this test to use assertions &
-// exceptions instead of return codes 
+// exceptions instead of return codes
 var r = test();
 assert (r === 0, r);
 
