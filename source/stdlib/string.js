b5f721b862bf6b4518e4708d3c1bde2a16b7b67b
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2014-11-07 00:19:15 -0500

    - Optimized String.fromCharCode

--- source/stdlib/string.js
+++ source/stdlib/string.js
@@ -124,6 +124,25 @@ function string_internal_isWhiteSpace(c)
            (c === 12288) || (c === 65279);
 }
 
+/// Preallocated character strings for 8-bit char codes
+$rt_char_str_table = (function () 
+{
+    var len = 256;
+    var table = $rt_arrtbl_alloc(len);
+
+    for (var c = 0; c < len; c++)
+    {
+        var str = $rt_str_alloc(1);
+        $rt_str_set_data(str, 0, c);
+        str = $ir_get_str(str);
+
+        $rt_arrtbl_set_word(table, c, $ir_get_word(str))
+        $rt_arrtbl_set_tag(table, c, $ir_get_tag(str))
+    }
+
+    return table;
+})();
+
 //-----------------------------------------------------------------------------
 
 /**
@@ -131,17 +150,43 @@ function string_internal_isWhiteSpace(c)
 */
 function string_fromCharCode(c)
 {
-    if ($ir_eq_i32($argc, 1) && $ir_is_int32(c))
+    if ($ir_eq_i32($argc, 1))
     {
+        // If this is a floating-point number safely convertible to an
+        // integer and within the character table range
+        if ($ir_is_float64(c) && $ir_ge_f64(c, 0.0) && $ir_lt_f64(c, 256.0))
+        {
+            c = $ir_f64_to_i32(c);
+
+            return $ir_make_value(
+                $rt_arrtbl_get_word($rt_char_str_table, c),
+                $rt_arrtbl_get_tag($rt_char_str_table, c)
+            );
+        }
+
+        // If this is a an integer within the character table range
+        if ($ir_is_int32(c) && $ir_ge_i32(c, 0) && $ir_lt_i32(c, 256))
+        {
+            return $ir_make_value(
+                $rt_arrtbl_get_word($rt_char_str_table, c),
+                $rt_arrtbl_get_tag($rt_char_str_table, c)
+            );
+        }
+
+        /*
         var str = $rt_str_alloc(1);
         $rt_str_set_data(str, 0, c);
         return $ir_get_str(str);
+        */
     }
 
-    var args = Array.prototype.slice.call(arguments, 0);
-    for(var i = 0; i < args.length; i++)
-        args[i] = parseInt(args[i]);
-    return string_internal_fromCharCodeArray(args);
+    var str = $rt_str_alloc($argc);
+
+    // TODO: use toUint32 and cap to 0xFFFF, parseInt is dog slow!
+    for (var i = 0; i < $argc; ++i)
+        $rt_str_set_data(str, i, parseInt($ir_get_arg(i)));
+
+    return $ir_get_str(str);
 }
 
 /**
