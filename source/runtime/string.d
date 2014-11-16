6185e14e61636fbb06618d3cb065fec9a160d534
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2014-11-16 10:17:17 -0500

    - Minor stylistic changes

--- source/runtime/string.d
+++ source/runtime/string.d
@@ -100,46 +100,46 @@ All hail Austin Appleby
 */
 uint64_t murmurHash64A(const void* key, size_t len, uint64_t seed = 1337)
 {
-	const uint64_t m = 0xc6a4a7935bd1e995;
-	const int r = 47;
+    const uint64_t m = 0xc6a4a7935bd1e995;
+    const int r = 47;
 
-	uint64_t h = seed ^ (len * m);
+    uint64_t h = seed ^ (len * m);
 
-  uint64_t* data = cast(uint64_t*)key;
-	uint64_t* end = data + (len/8);
+    uint64_t* data = cast(uint64_t*)key;
+    uint64_t* end = data + (len/8);
 
-	while (data != end)
-	{
-		uint64_t k = *data++;
+    while (data != end)
+    {
+        uint64_t k = *data++;
 
-		k *= m;
-		k ^= k >> r;
-		k *= m;
+        k *= m;
+        k ^= k >> r;
+        k *= m;
 
-		h ^= k;
-		h *= m;
-	}
+        h ^= k;
+        h *= m;
+    }
 
-	ubyte* tail = cast(ubyte*)data;
+    ubyte* tail = cast(ubyte*)data;
 
-	switch (len & 7)
-	{
-	    case 7: h ^= cast(uint64_t) tail[6] << 48;
-	    case 6: h ^= cast(uint64_t) tail[5] << 40;
-	    case 5: h ^= cast(uint64_t) tail[4] << 32;
-	    case 4: h ^= cast(uint64_t) tail[3] << 24;
-	    case 3: h ^= cast(uint64_t) tail[2] << 16;
-	    case 2: h ^= cast(uint64_t) tail[1] << 8;
-	    case 1: h ^= cast(uint64_t) tail[0];
-	            h *= m;
+    switch (len & 7)
+    {
+        case 7: h ^= (cast(uint64_t)tail[6]) << 48;
+        case 6: h ^= (cast(uint64_t)tail[5]) << 40;
+        case 5: h ^= (cast(uint64_t)tail[4]) << 32;
+        case 4: h ^= (cast(uint64_t)tail[3]) << 24;
+        case 3: h ^= (cast(uint64_t)tail[2]) << 16;
+        case 2: h ^= (cast(uint64_t)tail[1]) << 8;
+        case 1: h ^= (cast(uint64_t)tail[0]);
+                h *= m;
         default:
-	};
- 
-	h ^= h >> r;
-	h *= m;
-	h ^= h >> r;
+    };
 
-	return h;
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
 }
 
 /**
