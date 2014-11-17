366773327df58ddc07f8b96014af00875145502f
Ian Kronquist <iankronquist@teleport.com>
2014-11-16 21:19:59 -0800

    Add spidermonkey js test suite sans Intl
    I copied /js/src/tests to into /source/spidermonkey-tests
    I removed /js/src/tests/Intl because it depends on the Intl object which
    is baked into the js engine.
    The version I copied was changeset 215929:2a3399e38768 from the Mozilla inbound
    repository.
    According to my understanding, this code is available under the Mozilla
    Public License, which allows including the code as part of a "Larger
    Work", such as Higgs. I copied /toolkit/content/license.html to
    /source/spidermonkey-tests/license.html.

---
+++ source/spidermonkey-tests/js1_8/regress/regress-469625-03.js
@@ -0,0 +1,43 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*- */
+/*
+ * Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/licenses/publicdomain/
+ * Contributor: Jason Orendorff
+ */
+
+//-----------------------------------------------------------------------------
+var BUGNUMBER = 469625;
+var summary = 'Do not assert: script->objectsOffset != 0';
+var actual = '';
+var expect = '';
+
+
+//-----------------------------------------------------------------------------
+test();
+//-----------------------------------------------------------------------------
+
+function test()
+{
+  enterFunc ('test');
+  printBugNumber(BUGNUMBER);
+  printStatus (summary);
+ 
+  function f(x) {
+    var [a, b, [c0, c1]] = [x, x, x];
+  }
+
+  var ITERATOR = JS_HAS_SYMBOLS ? "Symbol.iterator" : "'@@iterator'";
+  expect = `TypeError: (intermediate value)[${ITERATOR}](...).next(...).value is null`;
+  actual = 'No Error';
+  try
+  {
+    f(null);
+  }
+  catch(ex)
+  {
+    actual = ex + '';
+  }
+  reportCompare(expect, actual, summary);
+  
+  exitFunc ('test');
+}
