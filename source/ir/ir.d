a854d395403050abef699dd7bc28db31fe4decac
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-03-26 23:56:50 -0400

    = Progress on C callbacks implementation

--- source/ir/ir.d
+++ source/ir/ir.d
@@ -5,7 +5,7 @@
 *  This file is part of the Higgs project. The project is distributed at:
 *  https://github.com/maximecb/Higgs
 *
-*  Copyright (c) 2011-2014, Maxime Chevalier-Boisvert. All rights reserved.
+*  Copyright (c) 2011-2015, Maxime Chevalier-Boisvert. All rights reserved.
 *
 *  This software is licensed under the following license (Modified BSD
 *  License):
@@ -124,8 +124,11 @@ class IRFunction : IdObject
     /// Type analysis results (may be null)
     TypeProp typeInfo = null;
 
-    /// Regular entry point code
+    /// Generic function entry point
     CodePtr entryCode = null;
+
+    /// C entry point code
+    CodeBlock cEntryCode;
 
     /// Map of blocks to lists of existing versions
     BlockVersion[][IRBlock] versionMap;
