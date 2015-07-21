b12c6fc9a49c5a904aa39aee6f3f6ae58a95f330
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-07-21 01:18:45 -0400

    - Modified microbenchmarks

--- source/benchmarks/micro/200m-calls.js
+++ source/benchmarks/micro/200m-calls.js
@@ -1,13 +1,17 @@
-function fun()
+function fun(x, y)
 {
+    for (var i = 0; i < 0; ++i)
+    {
+    }
 }
 
 function test()
 {
     for (var i = 0; $ir_lt_i32(i, 200000000); i = $ir_add_i32(i, 1))
     {
-        fun();
+        fun(i, 1);
     }
 }
 
 test();
+
--- source/benchmarks/micro/200m-primcalls.js
+++ source/benchmarks/micro/200m-primcalls.js
@@ -1,13 +1,17 @@
-function $rt_prim()
+function $rt_prim(x, y)
 {
+    for (var i = 0; i < 0; ++i)
+    {
+    }
 }
 
 function test()
 {
     for (var i = 0; $ir_lt_i32(i, 200000000); i = $ir_add_i32(i, 1))
     {
-        $rt_prim();
+        $rt_prim(i, 1);
     }
 }
 
 test();
+
