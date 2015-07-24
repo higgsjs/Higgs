d4bd6b52ebced3b33b67230392f8ce0489f91f54
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-07-24 16:49:23 -0400

    - Added microbenchmarks

---
+++ source/benchmarks/micro/200m-calls-local.js
@@ -0,0 +1,17 @@
+function test()
+{
+    function fun(x, y)
+    {
+        for (var i = 0; i < 0; ++i)
+        {
+        }
+    }
+
+    for (var i = 0; $ir_lt_i32(i, 200000000); i = $ir_add_i32(i, 1))
+    {
+        fun(i, 1);
+    }
+}
+
+test();
+
---
+++ source/benchmarks/micro/fib40.c
@@ -0,0 +1,15 @@
+int fib(int n)
+{
+    asm("");
+
+    if (n < 2)
+        return n;
+
+    return fib(n-1) + fib(n-2);
+}
+
+void main()
+{
+    fib(40);
+}
+
