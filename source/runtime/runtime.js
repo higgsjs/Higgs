5067e826f5b4e6343e9e5242d80770c6d0e52f06
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-11-23 22:06:46 -0500

    - Minor performance improvement on spectral-norm benchmark

--- source/runtime/runtime.js
+++ source/runtime/runtime.js
@@ -1230,9 +1230,15 @@ function $rt_divIntFloat(x, y)
             var r = $ir_div_i32(x, y);
 
             // Verify that there was no remainder
+            // Note: mul is much faster than div
             var v = $ir_mul_i32(r, y);
             if ($ir_eq_i32(x, v))
                 return r;
+
+            // Convert the integer values to floats
+            var fx = $ir_i32_to_f64(x);
+            var fy = $ir_i32_to_f64(y);
+            return $ir_div_f64(fx, fy);
         }
 
         if ($ir_is_float64(y))
