cd48d065e9f78c3de310a31b7027d9e7947d2ca4
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-11-24 15:15:18 -0500

    - 2x speedup on cordic benchmark

--- source/runtime/runtime.js
+++ source/runtime/runtime.js
@@ -1376,10 +1376,24 @@ function $rt_lsft(x, y)
 
 function $rt_rsft(x, y)
 {
-    // If both values are integer
-    if ($ir_is_int32(x) && $ir_is_int32(y))
+    // If x is integer
+    if ($ir_is_int32(x))
     {
-        return $ir_rsft_i32(x, y);
+        // If y is integer
+        if ($ir_is_int32(y))
+            return $ir_rsft_i32(x, y);
+    }
+
+    // If x is floating-point
+    if ($ir_is_float64(x) &&
+        $ir_ge_f64(x, 0.0) &&
+        $ir_le_f64(x, 512000000.0))
+    {
+        // If y is integer
+        if ($ir_is_int32(y))
+        {
+            return $ir_rsft_i32($ir_f64_to_i32(x), y);
+        }
     }
 
     // Convert the operands to integers
