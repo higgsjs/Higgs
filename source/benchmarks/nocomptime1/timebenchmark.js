8587398d51c73d20d6b0922f28ab2d62883fd28a
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2015-11-25 20:26:46 -0500

    - Added single exec benchmark suite

---
+++ source/benchmarks/nocomptime1/timebenchmark.js
@@ -0,0 +1,34 @@
+// By performing warmup runs, we abstract out compilation time, standard
+// library and runtime initialization time, as well as part of the benchmark
+// initialization time (global function definitions). We cannot remove garbage
+// collection time from the final timing run, however.
+
+function timeFun(fun, numItrs)
+{
+    var startTime = (new Date()).getTime();
+
+    for (var i = 0; i < numItrs; ++i)
+        fun();
+
+    var endTime = (new Date()).getTime();
+
+    return endTime - startTime;
+}
+
+if (typeof benchmarkFun != 'function')
+    throw Error('benchmarkFun not defined!');
+
+// Benchmarking time (to be measured)
+var benchTime = 0.0;
+
+// Number of timing iterations
+var numItrs = 1;
+
+// No warmup iterations
+
+// Time the benchmark over 1 iteration
+benchTime = timeFun(benchmarkFun, numItrs);
+
+print('num itrs:', numItrs);
+print('exec time (ms):', benchTime);
+
